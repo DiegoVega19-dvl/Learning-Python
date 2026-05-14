e1d7072878e9fa9c786d576dc966932e8b406d46
Juan Diego <diegovegaprogra@gmail.com>
2026-05-13 20:34:11 -0700

    -- clases --

--- POO/clases.py
+++ POO/clases.py
@@ -1,4 +1,4 @@
-class Automovil:
+"""class Automovil:
     # constructor de la clase
     def __init__(self, marca, modelo, año):
         self.marca = marca
@@ -12,7 +12,46 @@ class Automovil:
         print(f"Año: {self.año}")
 
 
+class Motocicleta:
+    def __init__(self, marca, modelo, color):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+
+
 # Crear una instancia de la clase Automovil
 mi_auto = Automovil("Toyota", "Corolla", 2020)
 # Mostrar la información del automóvil
-mi_auto.mostrar_informacion()
+mi_auto.mostrar_informacion()"""
+
+
+class Pelicula:
+    def __init__(self, titulo, duracion, genero):
+        self.titulo = titulo
+        self.duracion = duracion
+        self. genero = genero
+
+    def __str__(self):
+        return '{}({})'.format(self.titulo, self.duracion)
+
+
+class Catalogo:
+    peliculas = []
+
+    def __init__(self, peliculas=[]):
+        self.peliculas = peliculas
+
+    def agregar(self, p):
+        self.peliculas.append(p)
+
+    def mostrar(self):
+        for p in self.peliculas:
+            print(p)
+
+
+peli = Pelicula("Naufrago", 143, "supervivencia")
+peli2 = Pelicula("Atrapame si puedes", 141, "Caper")
+
+cata = Catalogo([peli])
+cata.agregar(Pelicula("atrapame si puedes", 141, "drama"))
+print(cata.mostrar())
--- manejo_errores/excepciones.py
+++ manejo_errores/excepciones.py
@@ -7,11 +7,80 @@
 
 print(dividir(20, 0)) """
 
-while (True):
+"""while (True):
     try:
         n = int(input("introduce un numero: "))
         m = int(input("introduce otro numero: "))
         print(n/m)
         break
     except Exception as e:
+        print(type(e).__name__)"""
+
+
+# invocacion de excepciones
+
+"""def funcion(algo=None):
+    if algo is None:
+        raise ValueError("Error no se permiten valores nulos")
+    
+
+
+funcion()"""
+
+
+# ejercicio 1: divison entre 0
+
+def division(a, b):
+    try:
+        return a/b
+    except Exception as e:
         print(type(e).__name__)
+
+
+print(division(100, 10))
+
+# ejercicio 2: indice
+
+lista = [1, 2, 3, 4, 5]
+
+try:
+    lista[10]
+except IndexError:
+    print("el indice que quieres acceder no existe dentro de la lista")
+
+
+# ejercicio 3: diccionario de datos
+
+colores = {'azul': 'blue', 'rojo': 'red', 'verde': 'green'}
+
+try:
+    colores['cafe']
+except KeyError:
+    print("el color que quieres acceder no existe")
+
+
+# ejercicio 4: type error
+
+try:
+    resultado = "15" + 20
+except TypeError:
+    print("solo se pueden sumar numeros del mismo tipo osea... enteros")
+
+
+# ejercicio 5: elementos de la lista
+
+ele = [0, 10, -2]
+
+
+def agregar(lista, elemento):
+    try:
+        if elemento in lista:
+            raise ValueError
+        else:
+            lista.append(elemento)
+    except ValueError:
+        print("no puede agregar numero duplicados => {}".format(elemento))
+
+
+print(agregar(ele, 5))
+print(ele)
